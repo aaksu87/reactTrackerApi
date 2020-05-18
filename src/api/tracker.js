80fd8af72e2a17a54b794b51a26dc7dccd147f29
akinaksu <akin@teknasyon.com>
2020-05-19 00:12:07 +0300

    heroku url added

--- src/api/tracker.js
+++ src/api/tracker.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import {AsyncStorage } from 'react-native';
 
 const instance =  axios.create({
-    baseURL : 'http://8ea651c1.ngrok.io'
+    baseURL : 'https://react-tracks-server.herokuapp.com'
 });
 
 instance.interceptors.request.use(
